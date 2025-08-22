7021ae00b61865b5010ae23db58a986ac6dad302
Duong <155739353+duongdh710@users.noreply.github.com>
2025-08-22 16:24:05 +0700

    Update

--- foundations/data_augmentation.py
+++ foundations/data_augmentation.py
@@ -8,16 +8,16 @@ img = Image.open(img_path)
 
 transform = transforms.Compose([
     transforms.RandomRotation(30),         
-    transforms.RandomHorizontalFlip(p=0.5), 
-    transforms.ColorJitter(brightness=0.3), 
-    transforms.RandomResizedCrop(224, scale=(0.8, 1.0)) 
+    transforms.RandomHorizontalFlip(p = 0.5), 
+    transforms.ColorJitter(brightness = 0.3), 
+    transforms.RandomResizedCrop(224, scale = (0.8, 1.0)) 
 ])
 
-fig, axs = plt.subplots(1, 6, figsize=(15, 5))
+fig, axs = plt.subplots(1, 6, figsize = (15, 5))
 axs[0].imshow(img)
 axs[0].axis("off")
 axs[0].set_title("Original")
-for i in range(1,6):
+for i in range(1, 6):
     aug_img = transform(img)
     axs[i].imshow(aug_img)
     axs[i].axis("off")
